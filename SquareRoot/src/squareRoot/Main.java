8edb9c4aac35ef4fff4b8851ebb5f143a402b0fb
MariaIvanova01 <mimi200801@abv.bg>
2022-08-03 15:27:03 +0300

    Using try-catch

--- SquareRoot/src/squareRoot/Main.java
+++ SquareRoot/src/squareRoot/Main.java
@@ -1,8 +1,33 @@
 package squareRoot;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        double sqrt = -1;
+
+        try {
+            sqrt = sqrt(Integer.parseInt(input));
+        }catch (IllegalArgumentException e){
+            System.out.println("Invalid");
+        }
+
+        if (sqrt != -1){
+            System.out.printf("%.2f%n",sqrt);
+        }
+        System.out.println("Goodbye");
+
+    }
+    public static double sqrt(int n){
+        if (n < 0){
+            throw new IllegalArgumentException("Invalid");
+
+        }
+        return Math.sqrt(n);
     }
 }
